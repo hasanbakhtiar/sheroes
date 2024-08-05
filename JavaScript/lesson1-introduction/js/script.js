a951093175b25e4d0fc7c1ddebebfa2625575164
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-08-05 10:04:20 +0400

    start js

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,11 @@
+// one line 
+
+/**
+ * more
+ * line
+ */
+
+// console.log("Hello JS");
+// console.log("Hello JS");
+// console.log("Hello JS");
+// console.log("Hello JS");
---
+++ JavaScript/lesson2-datatypes/js/script.js
@@ -0,0 +1,28 @@
+// Primitive DT
+// String DT
+var infoOne = "Hello";
+// Number DT
+var infoTwo = 5;
+// Boolean DT
+var infoThree = true; // false;
+
+// console.log(infoThree);
+
+// Composite DT 
+// Array
+var infoFour = ["Hello",5,true];
+// Object
+var infoFive = {
+    title:"BMW X5",
+    price:40000,
+    newCar:true
+}
+
+// Special DT
+// undefined 
+// null
+
+// var infoSix = null;
+
+
+// console.log(infoSix);
