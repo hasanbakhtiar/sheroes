1992302e71f5ddb868e67c6a381da764ba91b0ba
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-08-16 16:55:56 +0400

    other prop

---
+++ JavaScript/lesson8-other-prop/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+
+// const info = ["Table","Pen","Pencil","Pen filter"];
+// const data = ["Car","Train","Plane"];
+
+// const alldata = data.concat(info);
+// console.log(alldata.join("|"));
+// console.log(alldata.fill("Hello"));
+
+// info.push();
+// info.pop();
+// info.shift();
+// info.unshift("New")
+
+// console.log(info);
+
+
+
+
+
+// const myNumber = Math.round(Math.random()*5);
+
+// const Sheroes = ["Nazrin","Aysu","Meryem","Firuza","Naila","Minura","Hasan"]; 
+
+// console.log(Sheroes[myNumber]);
+
+
+// const myNumber = 5.23423;
+// console.log(myNumber.toExponential());
+
+
+
+ for(let i =0; i<10;i++){
+    if (i===5) {
+        continue;
+    }
+    
+    console.log(i);
+    
+ }
