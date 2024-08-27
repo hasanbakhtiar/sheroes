5b125ae1db9c691bf548b20f46812202815accad
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-08-27 17:05:16 +0400

    localstorage

---
+++ JavaScript/lesson11-dom-part3/js/script.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const modeBtn  = document.querySelector('#mode');
+// const nav  = document.querySelector('nav');
+
+
+// modeBtn.onclick = ()=>{
+//     if (modeBtn.innerHTML == "Dark") {
+//         nav.className = "navbar navbar-expand-lg bg-dark navbar-dark";
+//         modeBtn.innerHTML = "Light";
+//         localStorage.setItem('mode','dark');
+//     }else{
+//         nav.className = "navbar navbar-expand-lg bg-light navbar-light"
+//         modeBtn.innerHTML = "Dark";
+//         localStorage.setItem('mode','light');
+//     }
+// }
+
+
+// if (localStorage.getItem('mode') == "light") {
+//     nav.className = "navbar navbar-expand-lg bg-light navbar-light"
+//         modeBtn.innerHTML = "Dark";
+// }else{
+//     nav.className = "navbar navbar-expand-lg bg-dark navbar-dark";
+//     modeBtn.innerHTML = "Light";
+// }
+
+
+document.querySelector('button').onclick = ()=>{
+    // document.querySelector('h1').classList.add('red');
+    // document.querySelector('h1').classList.remove('red');
+    // document.querySelector('h1').classList.toggle('red');
+}
+
+
+
+
+
+// console.log(localStorage);
+
+// localStorage.setItem('mytextOne',"Hello One");
+// localStorage.setItem('mytextTwo',"Hello Two");
+// localStorage.clear();
+// localStorage.removeItem('mytextTwo');
+
+
+// document.querySelector('h1').innerHTML = localStorage.getItem('mytext');
+// document.querySelector('button').onclick = ()=>{
+//     localStorage.removeItem("mytext");
+//     window.location.reload();
+// }
