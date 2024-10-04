5262637a41e920dc75e759d2c5ccecf0e665926b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-10-04 17:00:39 +0400

    github finder

--- ReactJS/lesson7-githubfinder/src/components/Search.jsx
+++ ReactJS/lesson7-githubfinder/src/components/Search.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from 'react'
-
-const Search = ({comingData}) => {
-    const [keyword,setKeyword] = useState(null);
-
-    const formSubmit = e =>{
+import swal from 'sweetalert';
+import { ToastContainer, toast,Flip } from 'react-toastify';
+const Search = ({ comingData }) => {
+    const [keyword, setKeyword] = useState(null);
+    const notify = () => toast.warn("Please fill input!");
+    const formSubmit = e => {
         e.preventDefault();
-        comingData(keyword);
-        setKeyword("");
+        if (!keyword) {
+            // swal ( "Oops" ,  "Please fill input!" ,  "warning" )
+            notify();
+        } else {
+            comingData(keyword);
+            setKeyword("");
+        }
     }
-    
+
     return (
         <div className="d-flex align-items-center justify-content-center my-5">
             <div className="col-6">
                 <form onSubmit={formSubmit} className="input-group mb-3">
-                    <input value={keyword} onChange={e=>setKeyword(e.target.value)} type="text" className="form-control" placeholder="Enter username"  />
+                    <input value={keyword} onChange={e => setKeyword(e.target.value)} type="text" className="form-control" placeholder="Enter username" />
                     <button className="btn btn-dark" type="submit" >Search</button>
+                    <ToastContainer
+                    position="bottom-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="colored"
+                    transition= {Flip}
+/>
                 </form>
 
             </div>
--- ReactJS/lesson7-githubfinder/src/main.jsx
+++ ReactJS/lesson7-githubfinder/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App'
 
 
