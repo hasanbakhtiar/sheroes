c5202c517b6b05f34dad3da6ecfe2721b9c95f63
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-10-22 17:17:46 +0400

    redux start

--- ReduxJS/lesson1-introduction/src/App.jsx
+++ ReduxJS/lesson1-introduction/src/App.jsx
@@ -7,7 +7,11 @@ const store = createStore((state=0, action) => {
     case "increment":
       return state+1;
     case "decrement":
-      return state-1;
+        if (state>0) {
+          return state-1;
+        }else{
+          return "no";
+        }
 
     case "reset":
       return state=0
@@ -17,15 +21,59 @@ const store = createStore((state=0, action) => {
       return state;
   }
 });
-
-store.dispatch({
-  type:"reset"
-})
-store.dispatch({
-  type:"decrement"
+store.subscribe(()=>{
+  console.log(store.getState());
 })
 
-console.log(store.getState());
+store.dispatch({type:'increment'})
+store.dispatch({type:'increment'})
+store.dispatch({type:'increment'})
+store.dispatch({type:'decrement'})
+store.dispatch({type:'decrement'})
+store.dispatch({type:'decrement'})
+store.dispatch({type:'decrement'})
+
+
+
+
+
+// const store = createStore((state=0, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return state+action.payload;
+//     case "decrement":
+//         if (state>0) {
+//           return state-(typeof(action.payload)=="string" ?0:action.payload);
+//         }else{
+//           return "no";
+//         }
+
+//     case "reset":
+//       return state=0
+
+
+//     default:
+//       return state;
+//   }
+// });
+
+
+// store.dispatch({
+//   type:"increment",
+//   payload:5
+// })
+
+// store.dispatch({
+//   type:"decrement",
+//   payload:5
+// })
+// store.dispatch({
+//   type:"decrement",
+//   payload:10
+// })
+// console.log(store.getState());
+
+
 
 
 
